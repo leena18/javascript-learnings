06d8222cc0e265a60c2abdb8eae618d4f7241f74
leena ghatiya <leena@valuebound.com>
2024-11-01 20:14:13 +0530

    till datatype

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail= "leena@google.com"
+var accountPassword="12345"
+accountCity="Indore"
+
+
+/* prefer not to use var because of issue in block scope and functional scope */
+
+accountId = 2
+console.log(accountId);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+// "use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+//console.log(3+3) // code readability should be high
+
+// "33"=> 33
+// "33abc" => this is NaN not a number
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false
+// ""=> false
+// "leena"=> true
+
+let someomeNumber = 33
+let stringNumber = String(someomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
